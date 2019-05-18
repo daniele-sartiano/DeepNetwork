01521653cd62a526fffcb8ce4607a53dadd1a3b0
Daniele <daniele.sartiano@gmail.com>
2019-05-18 18:52:02 +0200

    Added hosts and n_flows

--- reader/reader.py
+++ reader/reader.py
@@ -3,11 +3,14 @@ from abc import ABC, abstractmethod
 class Reader(ABC):
     def __init__(self, input):
         self.input = input
-
+        self.hosts = set()
+        
     @abstractmethod
     def read(self):
         pass
 
 
 class FlowReader(Reader):
-    pass
+    def __init__(self, input):
+        super().__init__(input)
+        self.n_flows = 0
